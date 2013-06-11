2c9afae98cc29175d445e8b9a8262a49f9a4acd2
Jiang XD <lq_8888@yeah.net>
2013-06-11 10:41:10 +0800

    daily update script

--- code/daily-update.sh
+++ code/daily-update.sh
@@ -7,3 +7,9 @@ sort ../cosdata.txt | uniq > tmp.txt
 mv tmp.txt ../cosdata.txt
 wc -l ../cosdata.txt
 
+echo
+
+git add ../cosdata.txt
+git add *.txt
+git commit -m "$(date)"
+git push origin master
