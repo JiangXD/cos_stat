80c4c04dc0819c93902d2262738219bf7687931f
Jiang XD <lq_8888@yeah.net>
2013-06-14 11:52:05 +0800

    figure support

--- code/authority-update.sh
+++ code/authority-update.sh
@@ -2,8 +2,10 @@
 
 echo "Conducting analysis..."
 Rscript authority_analysis.R
+mv figure ../
 echo "Uploading..."
 git add ../README.md
+git add ../figure/*.png
 git add *.Rmd
 git commit -m "$(date +%Y-%m-%d)"
 git push origin master
--- code/authority_analysis.R
+++ code/authority_analysis.R
@@ -1,4 +1,5 @@
 library("knitr")
+library("ggplot2")
 
 mydata = read.csv("../cosdata-mysql.csv")
 #today = as.Date(format(Sys.time(),"%Y-%m-%d"))
@@ -12,6 +13,13 @@ mytop = head(sort(mytable, decreasing=TRUE), n=20L)
 mytopposts = sum(mytop)
 myratio = mytopposts*100/myposts
 myreplies = table(mysub$is_topic_poster)
+
+tmp=table(mysub$poster_id, mysub$is_topic_poster)
+replies=tmp[,1]
+topics=tmp[,2]
+ids=names(tmp[,1])
+mysummary=data.frame(replies, topics, ids)
+
 
 knit("README.Rmd", output="../README.md")
 warnings()
